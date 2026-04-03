892714c25e374736edfcb8aae63b268372b2161a
Fabián Murgado <fabian@fabianmurgado.com>
2026-04-03 06:42:51 +0200

    chore(train): change default batch size to 16

--- src/minimal_llm/train.py
+++ src/minimal_llm/train.py
@@ -181,7 +181,7 @@ def parse_args() -> argparse.Namespace:
 
     # Training hyperparameters
     parser.add_argument("--max_steps", type=int, default=10000)
-    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--grad_accum_steps", type=int, default=1)
     parser.add_argument("--lr", type=float, default=3e-4)
     parser.add_argument("--weight_decay", type=float, default=0.1)
